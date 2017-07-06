c583c04098a1732c9be0f22e555a9f69e983c924
Murat Han Acıpayam <murathanacipayam@hotmail.com>
2017-07-06 10:26:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/murathanacpayam/bildirimcubugugizlemekodu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.murathanacpayam.bildirimcubugugizlemekodu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //----Bildirim Cubugu gizleme Kodu------------------------------------------------------
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //------------------------------------------------------------------------------------------------------------
+
+    }
+}
